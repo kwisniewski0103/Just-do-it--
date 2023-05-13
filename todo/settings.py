5dbdc55e778ab7270231fc1f7195228b1ffc66df
Kamil Wiśniewski <124616552+kwisniewski0103@users.noreply.github.com>
2023-05-13 12:39:05 +0200

    python anywhere

--- todo/settings.py
+++ todo/settings.py
@@ -10,7 +10,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["justdoit-kwisniewski0103.pythonanywhere.com"]
 
 INSTALLED_APPS = [
     'django.contrib.admin',
